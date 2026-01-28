ce15e8f3ae1ed99ba9bd199a05fd168ba2438cb9
Si1veR1123 <connorgeorgeem@aol.com>
2026-01-28 23:27:11 +0000

    server returns recent image and readings

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Reading(models.Model):
+    timestamp = models.DateTimeField(primary_key=True)
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+
+    class Meta:
+        db_table = "readings"
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return f"{self.timestamp}: {self.temperature}°C, {self.humidity}%"
